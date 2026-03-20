58d1a372b127731df0ec48d431f48019e3ea4cc7
Yubin Lee <yubin8938@gmail.com>
2026-03-21 01:03:02 +0900

    change logo (#11)

--- public/logo.tsx
+++ public/logo.tsx
@@ -1,9 +1,17 @@
-import TextLogo from "./textLogoSvg";
+import Image from "next/image";
+import { useTheme } from "next-themes";
 
 export default function Logo() {
+  const { theme } = useTheme();
+
   return (
     <div className="flex items-center translation-y-[2px]">
-      <TextLogo />
+      <Image
+        src={theme === "dark" ? "/logo_dark.png" : "/logo.png"}
+        alt="Logo"
+        width={100}
+        height={100}
+      />
     </div>
   );
 }
